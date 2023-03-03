681042ec0b64eb257da8b620b2cac016d82a79d6
dashbah <dsbakhankova@edu.hse.ru>
2023-03-03 17:14:32 +0300

    bulls and cows edited

--- Projects/BullsAndCows/BullsAndCows/Program.cs
+++ Projects/BullsAndCows/BullsAndCows/Program.cs
@@ -8,37 +8,29 @@ namespace PeerGrade
         /// Метод генерирует массив, создает из него число.
         /// </summary>
         /// <returns> возвращает строковое представление числа.</returns>
-        public static string Number()
+        public static int Number()
         {
-            //По умолчанию массив заполняется нулями. Тогда при генерации все нули будут удаляться как повторяющиеся.
-            //Поэтому его нужно заполнить.
-            int[] number = new int[4] { 10, 10, 10, 10 };
+            int[] number = new int[4];
             Random rnd = new Random();
-            int rpt;
-            // l- вспомогательная переменная. Генерация цифры идет, пока l=1. Если цифра уже была в числе, 
-            // т.е. rpt>0, то l остается равной 1. Если же цифра новая, то l=0, переходим к новой цифре.
-            int l;
-            for (int i = 0; i < 4; i++)
+            number[0] =  rnd.Next(1, 9);
+            for (int i = 0; i < 3; ++i)
             {
-                l = 1;
-                while (l == 1)
+                bool exists = false;
+                do
                 {
-                    if (i == 0)
-                        number[i] = rnd.Next(1, 9);
-                    else
-                        number[i] = rnd.Next(9);
-                    rpt = 0;
-                    for (int j = 0; j < 4; j++)
-                        if (number[i] == number[j] && i != j)
-                            rpt++;
-                    if (rpt == 0)
-                        l = 0;
-                }
+                    number[i] = rnd.Next(9);
+                    for (int j = 0; j < i; ++j)
+                    {
+                        if (number[i] == number[j])
+                        {
+                            exists = true;
+                            break;
+                        }
+                    }
+                } while (exists);
             }
-            int result = number[0] * 1000 + number[1] * 100 + number[2] * 10 + number[3];
-            //Перевод в строковый тип, так как Check будет сравнивать строки.
-            string result1 = result.ToString();
-            return result1;
+            Console.Write(number[0] * 1000 + number[1] * 100 + number[2] * 10 + number[3]);
+            return number[0] * 1000 + number[1] * 100 + number[2] * 10 + number[3];
         }
 
         /// <summary>
@@ -62,27 +54,19 @@ namespace PeerGrade
             Console.WriteLine("быки: " + a);
             Console.WriteLine("коровы: " + b);
         }
-
-        /// <summary>
-        /// Точка входа.
-        /// </summary>
-        /// <param name="args"></param>
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Быки - количество угаданных и находящихся на своих местах цифр.");
             Console.WriteLine("Коровы - количество угаданных, но не расположенных на своих местах.");
             Console.WriteLine();
-            //repeat отвечает за то, чтобы пользователь мог перезапустить игру.
             bool repeat = true;
             while (repeat)
             {
                 Console.WriteLine("Введите четырехзначное число, цифры не должны повторяться:");
-                //Генерация числа.
-                string value = Number();
-                //Введенное пользователем число.
-                string entering = "0";
-                //Цикл выполнется, пока число не будет угадано.
-                while (value != entering)
+                string value = Number().ToString();
+                string entering;
+                do
                 {
                     entering = Console.ReadLine();
                     if (!int.TryParse(entering, out int a) || entering.Length != 4)
@@ -90,16 +74,11 @@ namespace PeerGrade
                     else
                         Check(entering, value);
                     Console.WriteLine();
-                }
+                } while (value != entering);
+                
                 Console.WriteLine("Поздравляю, Вы угадали!");
                 Console.WriteLine("Чтобы начать заново, нажмите пробел. Для выхода нажмите любую другую клавишу.");
-                if (Console.ReadKey().Key == ConsoleKey.Spacebar)
-                {
-                    repeat = true;
-                    Console.WriteLine();
-                }
-                else
-                    repeat = false;
+                repeat = Console.ReadKey().Key == ConsoleKey.Spacebar;
             }
         }
     }
